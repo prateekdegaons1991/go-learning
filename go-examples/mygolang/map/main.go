0b555c36fd10882439992d686b7b30d054b4c611
pratikdegaon <prateekdegaons1991@outlook.com>
2024-05-05 21:17:12 +0530

    add maps

---
+++ go-examples/mygolang/map/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	colors := map[string]string{
+		"Red":    "#ff00",
+		"Green":  "#ffkh",
+		"Yellow": "#ffoo1",
+		"Blue":   "#ff0002",
+	}
+
+	fmt.Printf("colors: %+v", colors)
+}
